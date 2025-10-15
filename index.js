623453ca8934a1e3ef3c2a77797d5d434de412c4
Anaswar4 <anaswarpk13@gmail.com>
2025-10-15 12:20:09 +0000

    fileMetaData

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
+var Path= require('path');
+var fs = require('fs');
 require('dotenv').config()
 
 var app = express();
@@ -7,14 +10,42 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+if(!fs.existsSync('./uploads')){
+  fs.mkdirSync('./uploads')
+}
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+//storage setup
+const storage = multer.diskStorage({
+  destination:function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req,file,cb){
+    cb(null,Date.now() + Path.extname(file.originalname))
+  }
+});
+
+const upload = multer({storage:storage})
+
+app.post('/api/fileanalyse',upload.single('upfile'),(req,res)=>{
+  const file = req.file;
+  if(!file){
+     return res.json({error:'Please upload a file'})
+  }
+  res.json({
+    name:file.originalname,
+    type:file.mimetype,
+    size:file.size
+  });
 });
 
 
 
 
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
+const port = process.env.PORT || 5000;
+app.listen(port, '0.0.0.0', function () {
   console.log('Your app is listening on port ' + port)
 });
